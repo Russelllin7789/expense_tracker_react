d89548a72076f5167f437d230674aa8a9dbaca20
RussellLin <superbr7789@gmail.com>
2022-01-16 19:26:42 +0800

    Render conditional component instead of filtered expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,21 +15,25 @@ const Expenses = (props) => {
   // render the expenses of selected year, rather than rendering the actual data
   const filteredExpenses = props.items.filter(expense => expense.date.getFullYear().toString() === year)
 
+  let expensesContent = <p>Oops! No related expense at all!</p>
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map(
+      expense =>
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+    )
+  }
+
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={year} onYearSelected={selectedYearHandler} />
-        {
-          filteredExpenses.map(
-            expense =>
-              <ExpenseItem
-                key={expense.id}
-                title={expense.title}
-                amount={expense.amount}
-                date={expense.date}
-              />
-          )
-        }
+        {expensesContent}
       </Card>
     </div>
   )
