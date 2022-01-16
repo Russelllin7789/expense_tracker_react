4ebb5f5a383f032615fffae319d0715826ec315f
RussellLin <superbr7789@gmail.com>
2022-01-16 19:45:46 +0800

    Extract the filtered expenses into expense list

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
 
+import Card from "../common UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from './ExpensesFilter'
+import ExpensesList from './ExpensesList'
 import './Expenses.css'
-import Card from "../common UI/Card";
 
 const Expenses = (props) => {
   const [year, setYear] = useState('2019')
@@ -15,25 +16,11 @@ const Expenses = (props) => {
   // render the expenses of selected year, rather than rendering the actual data
   const filteredExpenses = props.items.filter(expense => expense.date.getFullYear().toString() === year)
 
-  let expensesContent = <p>Oops! No related expense at all!</p>
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map(
-      expense =>
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-    )
-  }
-
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={year} onYearSelected={selectedYearHandler} />
-        {expensesContent}
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   )
