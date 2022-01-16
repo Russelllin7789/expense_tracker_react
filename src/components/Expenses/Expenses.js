4bfdcd61245f14024822ead7d97a04d4b9e67e09
RussellLin <superbr7789@gmail.com>
2022-01-16 17:05:18 +0800

    Dynamically rendered the expense array with some initial dummy data

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -17,7 +17,18 @@ const Expenses = (props) => {
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={year} onYearSelected={selectedYearHandler} />
-        <ExpenseItem
+        {
+          props.items.map(
+            expense =>
+              <ExpenseItem
+                key={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+              />
+          )
+        }
+        {/* <ExpenseItem
           title={props.items[0].title}
           amount={props.items[0].amount}
           date={props.items[0].date}
@@ -36,7 +47,7 @@ const Expenses = (props) => {
           title={props.items[3].title}
           amount={props.items[3].amount}
           date={props.items[3].date}
-        />
+        /> */}
       </Card>
     </div>
   )
