464fe884faebeb8bb9d72e3a64466a8f6d501e6d
RussellLin <superbr7789@gmail.com>
2022-01-19 21:31:09 +0800

    Add user form detail

--- src/components/User/AddUser.js
+++ src/components/User/AddUser.js
@@ -1,8 +1,14 @@
 import React from "react";
+import Card from '../common UI/Card'
 
-const AddUser = () => {
+const AddUser = (props) => {
   return (
-    
+    <form>
+      <label htmlFor="userName">User Name</label>
+      <input type="text" />
+      <label htmlFor="age">Age</label>
+      <input type="number" />
+    </form>
   )
 }
 
